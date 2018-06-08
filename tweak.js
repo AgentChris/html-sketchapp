1a743119abde35fe8309a925b40f967c7a1d073a
chris.lego <poputeacristi@gmail.com>
2018-06-08 16:34:31 +0300

    replace font

---
+++ tweak.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var fileName = './dist-sketchapp/page.asketch.json';
+var file = require(fileName);
+
+// file.key = "new value";
+let old_json_string = JSON.stringify(file);
+// console.log(old_json_string);
+const new_string = old_json_string.replace(/GeographWeb/g, 'Geograph');
+// console.log(old_json_string);
+console.log(new_string);
+
+fs.writeFile(fileName, new_string, function (err) {
+// fs.writeFile(fileName, JSON.stringify(file), function (err) {
+  if (err) return console.log(err);
+  console.log(new_string);
+  console.log('writing to ' + fileName);
+});
